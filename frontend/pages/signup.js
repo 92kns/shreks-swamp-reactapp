e86d4ad3b0959e4bfa5a2e48751721a02f276f61
kns92 <kshampur@gmail.com>
2020-12-12 18:45:50 -0500

    readme link

--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -3,7 +3,9 @@ import Link from 'next/link';
 const signup = props => (
     <div>
         <p>SORRY EXCLUSIVE MEMBERS OF THE SWAMP ONLY!</p>
-        <p> Just kidding. As mentioned in the readme, this is a demo site with minimal functionality. Now get outta my swamp! </p>
+        <p> Just kidding.
+            As mentioned in the <u><a href="https://github.com/92kns/shreks-swamp-reactapp/blob/main/README.md"  >README</a></u>, 
+            this is a demo site with minimal functionality. Now get outta my swamp! </p>
 
         <p>Or play around with uploading, editing, and deleting items!</p>
 
